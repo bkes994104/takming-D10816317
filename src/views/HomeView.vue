f71a8082d8b9688810f3094a5035c863685ed576
bkes994104 <bkes994104@gmail.com>
2022-06-01 16:54:42 +0800

    fix: init Home.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
   <div> 
+    <div>台北</div>
+    <h1>H1</h1>
+    <h2>H2</h2>
+    <h3>H3</h3>
+    <h4>H4</h4>
+    <h5>H5</h5>
+    <h6>H6</h6>
+    <p>P</p>
+    <span>span</span>
+    <small>small</small>
   </div>
 </template>
 
