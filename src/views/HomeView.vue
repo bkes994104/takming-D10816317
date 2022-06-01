9acf411110fec3e74fb480da4ccdc11f90f8f00a
bkes994104 <bkes994104@gmail.com>
2022-06-01 16:58:12 +0800

    fix:  set grid & title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,20 @@
 <template>
-  <div> 
-    <div>台北</div>
-    <h1>H1</h1>
-    <h2>H2</h2>
-    <h3>H3</h3>
-    <h4>H4</h4>
-    <h5>H5</h5>
-    <h6>H6</h6>
-    <p>P</p>
-    <span>span</span>
-    <small>small</small>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="text-center py-10">
+          人員清單
+        </h1>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-
 <script>
 export default {
   data() {
     return {};
-  },
+  }
 };
 </script>
+<style scoped>
+</style>
