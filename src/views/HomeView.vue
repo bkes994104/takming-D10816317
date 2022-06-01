6f03066e91a3c7e5d0024688a88848b0351a9155
bkes994104 <bkes994104@gmail.com>
2022-06-01 17:12:10 +0800

    fix: beautify portal card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,21 +9,21 @@
     </v-row>
     <v-row>
       <v-col md="4">
-        <v-card height="150px">
-          <div>
+        <v-card height="150px" class="px-5 py-3 d-flex flex-column rounded-lg">
+          <div class="d-flex align-center flex-grow-0">
             <h3 class="primary--text">
               王小明
             </h3>
-            <h4 class="gray">
+            <h4 class="gray--text ml-auto" >
               男
             </h4>
           </div>
-          <div>
+          <div class="flex-grow-0">
             <span class="gray--text">
               2021062011493428
             </span>
           </div>
-          <div>
+          <div class="d-flex align-end flex-grow-1">
             <v-icon left color="secondary">
               mdi-gmail
             </v-icon>
@@ -32,7 +32,6 @@
             </span>
           </div>
         </v-card>
-        
       </v-col>
     </v-row>
   </v-container>
