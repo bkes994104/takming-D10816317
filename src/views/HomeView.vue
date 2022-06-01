b478aa9a4e4add6afe7a3194536212e9cbf4b087
bkes994104 <bkes994104@gmail.com>
2022-06-01 17:48:19 +0800

    feat: user dialog

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-container>
+<div>
+   <v-container>
     <v-row>
       <v-col cols="12">
         <h1 class="text-center py-10">
@@ -8,14 +9,14 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col md="4" v-for="i in 9" :key="i">
+      <v-col md="4" cols="12" v-for="user in currentUsers" :key="user.UID">
         <v-card height="150px" class="px-5 py-3 d-flex flex-column rounded-lg">
           <div class="d-flex align-center flex-grow-0">
             <h3 class="primary--text">
               <strong>{{user.name}}</strong>
             </h3>
             <h4 class="gray--text ml-auto" >
-              <strong>{{user.gender}}</strong>
+              <strong>{{handleGender(user.gender)}}</strong>
             </h4>
           </div>
           <div class="flex-grow-0">
@@ -36,36 +37,51 @@
     </v-row>
     <v-row class="mt-8">
       <v-col cols="12">
-        <v-pagination 
-          :value="1" 
-          :length="6"
-        />
+        <v-pagination v-model="page" :length="totalPages" />
       </v-col>
     </v-row>
   </v-container>
+  <v-dialog v-model="dialog" width="535">
+
+  </v-dialog>
+</div>
+ 
 </template>
 <script>
+import admin from'../assets/admin.json';
 
 export default {
   data() {
     return {
-      user: {
-        name: '王小明',
-        UID: "2021062012003739",
-        SID: "ROOT",
-        CID: "",
-        name: "Am",
-        phone: "0987654334",
-        gender: "M",
-        email: "Takming014@takming.edu.tw",
-        zip: 300,
-        address: "新竹縣湖口鄉德和路228號",
-        createTime: "2021-06-20 12:00:37",
-        updateTime: "2021-06-20 12:00:37",
-        isDelete: null
-      }
+      dialog: false,
     };
+  },
+  created() {
+  this.getUsers();
+  },
+  methods: {
+    getUsers() {
+      this.users = admin.data.user;
+    },
+    handleGender(type) {
+      if (type === "M") return "男";
+      if (type === "W") return "女";
+      return "其他";
+    }
+  },
+  computed: {
+  //計算總頁數
+    totalPages() {
+      return Math.ceil(this.users.length / 9);
+    },
+    //計算目前頁面使用者們
+    currentUsers() {
+      const begin = (this.page - 1) * 9;
+      const end = begin + 9;
+      return this.users.slice(begin, end);
+    }
   }
+
 };
 </script>
 <style scoped>
